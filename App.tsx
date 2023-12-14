18323c30586924b088d34bb951558df17005f074
jauresmanouan <jauresmanouan@gmail.com>
2023-12-14 03:13:15 +0000

    Music notes add

--- App.tsx
+++ App.tsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 // Liste des notes de musique
-const notes = ['Do', 'Do#', 'Ré', 'Ré#', 'Mi', 'Mi#', 'Fa', 'Fa#', 
+const notes: string[] = ['Do', 'Do#', 'Ré', 'Ré#', 'Mi', 'Mi#', 'Fa', 'Fa#', 
               'Sol', 'Sol#', 'La', 'La#', 'Si', 'Si#'];
 
 // Fonction pour obtenir une note aléatoire
-const getRandomNote = () => {
-  const randomIndex = Math.floor(Math.random() * notes.length);
+const getRandomNote = (): string => {
+  const randomIndex: number = Math.floor(Math.random() * notes.length);
   return notes[randomIndex];
 };
 
 // Composant principal de l'application
-const MusicApp = () => {
+const MusicApp: React.FC = () => {
   // État pour stocker la note actuelle
-  const [currentNote, setCurrentNote] = useState('');
+  const [currentNote, setCurrentNote] = useState<string>('');
 
   // Utilisation de useEffect pour exécuter du code après le rendu initial
   useEffect(() => {
     // Définir un intervalle pour changer la note toutes les 2 secondes
-    const intervalId = setInterval(() => {
-      const randomNote = getRandomNote();
+    const intervalId: NodeJS.Timeout = setInterval(() => {
+      const randomNote: string = getRandomNote();
       setCurrentNote(randomNote);
     }, 3000);
 
@@ -34,8 +34,8 @@ const MusicApp = () => {
       <Text style={styles.noteText}>{currentNote}</Text>
     </View>
   );
-  
 };
+
 
 // Styles pour l'interface utilisateur
 const styles = StyleSheet.create({
@@ -43,18 +43,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: '#FF0016',
   },
   noteText: {
     fontSize: 150,
     fontFamily: 'Montserrat',
-    fontWeight:'bold',
+    fontWeight: 'bold',
     color: 'white',
     shadowColor: 'blue',
-    shadowOffset: {width: 7, height: 7},
+    shadowOffset: { width: 7, height: 7 },
     shadowOpacity: 1,
     shadowRadius: 0,
   },
-})
+});
 
 export default MusicApp;
